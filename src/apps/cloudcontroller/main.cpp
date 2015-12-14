cb760ab7dbb1ce4f940cfeccf3a8eda55d32693e
SOFTBOY <softboy@fhzc.com>
2015-12-14 12:23:14 +0800

    submodule update

--- src/apps/cloudcontroller/main.cpp
+++ src/apps/cloudcontroller/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-
+#include <QDebug>
 using namespace std;
 
 #include "command/simple_route_item.h"
+#include "ds/priority_list.h"
 
 int main()
 {
@@ -12,8 +13,13 @@ int main()
       "sdfasdfgsdjhfgsadjhfasdh",
       "--something"
    };
+   sn::corelib::PriorityList<int> list;
+   list.insert("b", 1, 3);
+   list.insert("c", 100, 1);
+   list.insert("a", 222, 2);
+   list.sort();
+   qDebug() << list[1];
    route.match(args);
    cout << "Hello World!" << endl;
    return 0;
 }
-
