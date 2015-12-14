26a40eb36555097b64aac85e4a558095878170cc
SOFTBOY <softboy@fhzc.com>
2015-12-14 17:07:59 +0800

    submodule update

--- src/apps/cloudcontroller/main.cpp
+++ src/apps/cloudcontroller/main.cpp
@@ -2,24 +2,27 @@
 #include <QDebug>
 using namespace std;
 
-#include "command/simple_route_item.h"
+#include "command/route_item.h"
 #include "ds/priority_list.h"
+#include "command/route_stack.h"
+#include "command/route_match_result.h"
 
 int main()
 {
    //sn::corelib::SimpleRouteItem route("find user [--id=] [--firstName=] [--lastName=] [--email=] [--position=]");
-   sn::corelib::SimpleRouteItem route("sdfasdfgsdjhfgsadjhfasdh --something");
+//   sn::corelib::SimpleRouteItem route("sdfasdfgsdjhfgsadjhfasdh --something");
    QStringList args{
       "sdfasdfgsdjhfgsadjhfasdh",
       "--something"
    };
-   sn::corelib::PriorityList<int> list;
-   list.insert("b", 1, 3);
-   list.insert("c", 100, 1);
-   list.insert("a", 222, 2);
-   list.sort();
-   qDebug() << list[1];
-   route.match(args);
+   sn::corelib::RouteStack router;
+   router.addRoute("first", sn::corelib::RouteItem("sdfasdfgsdjhfgsadjhfasdh --something",{
+                                                      {"controller", "asfdasdasdsa"}
+                                                   }));
+   sn::corelib::RouteMatchResult result = router.match(args);
+   
+   //router.match(args);
+//   route.match(args);
    cout << "Hello World!" << endl;
    return 0;
 }
