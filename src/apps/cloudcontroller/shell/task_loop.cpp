ec64f19e125e756afa613091aa2a9f33e0ce9cbd
SOFTBOY <softboy@fhzc.com>
2015-12-18 21:01:24 +0800

    修复相关漏斗

--- src/apps/cloudcontroller/shell/task_loop.cpp
+++ src/apps/cloudcontroller/shell/task_loop.cpp
@@ -3,6 +3,8 @@
 #include <QPair>
 #include <QRegularExpression>
 #include <QRegularExpressionMatch>
+#include <QList>
+#include <QChar>
 #include <QDebug>
 
 #include <cstdio>
@@ -26,13 +28,15 @@ using sn::corelib::ErrorInfo;
 
 const int ASCII_NUL = 0;
 const int ASCII_ESC = 27;
-
+const int UNICODE_RANGE_START = 0x4E00;
+const int UNICODE_RANGE_END = 0x9FA5;
 TaskLoop::TaskLoop()
 {
    QPair<int, int> winSize = Terminal::getWindowSize();
    m_windowWidth = winSize.first;
    m_windowHeight = winSize.second;
    setupTerminalAttr();
+   m_ps_length = calculateCursorStep(m_ps);
 }
 
 void TaskLoop::setupTerminalAttr()
@@ -46,7 +50,7 @@ void TaskLoop::setupTerminalAttr()
    terminalAttr.c_cc[VTIME] = 0;
    int status = tcsetattr(STDIN_FILENO, TCSANOW, &terminalAttr);
    Q_ASSERT_X(status == 0, "TaskLoop::setupTerminalAttr()", "tcsetattr error");
-  
+   
 }
 
 void TaskLoop::run()
@@ -67,14 +71,14 @@ void TaskLoop::updateTerminalWindowSize(int width, int height)
 {
    m_windowWidth = width;
    m_windowHeight = height;
-   refreshLine();
+   //   refreshLine();
 }
 
 void TaskLoop::readCommand(QString &command)
 {
    fd_set readFds;
-   char buf[32];
-   memset(buf, ASCII_NUL, 32);
+   char buf[128];
+   memset(buf, ASCII_NUL, 128);
    int nfds = 1;
    FD_ZERO(&readFds);
    FD_SET(STDIN_FILENO, &readFds);
@@ -88,33 +92,52 @@ LABEL_AGAIN:
    }
    for(int fd = 0; fd < nfds; fd++){
       if(FD_ISSET(fd, &readFds)){
-         read(STDIN_FILENO, &buf, 31);
-         QByteArray unit;
-         bool escReaded = false;
-         for(int i =0; i < 32; i++){
-            if(buf[i] == ASCII_ESC && !escReaded){
-               escReaded = true;
-            }else if((buf[i] == ASCII_ESC && escReaded) || buf[i] == ASCII_NUL){
-               break;
+         read(STDIN_FILENO, &buf, 127);
+         QString unit;
+         filterBuffer(buf, unit);
+         memset(buf, ASCII_NUL, 128);
+         if(!unit.isEmpty()){
+            SpecialKeyName keyType = getKeyTypeName(unit);
+            readUnitCycle(unit, keyType);
+            if(keyType == SpecialKeyName::ASCII_CODE && unit == "\n"){
+               command = m_cmd_buff.trimmed();
+               m_cmd_buff.clear();
+               m_insertPos = 0;
+               std::cout << "\n";
+               return;
             }
-            unit.append(buf[i]);
-         }
-         memset(buf, ASCII_NUL, 32);
-         SpecialKeyName keyType = getKeyTypeName(unit);
-         readUnitCycle(unit, keyType);
-         if(keyType == SpecialKeyName::ASCII_CODE && unit == "\n"){
-            command = m_cmd_buff.trimmed();
-            m_cmd_buff.clear();
-            m_insertPos = 0;
-            std::cout << "\n";
-            return;
          }
          goto LABEL_AGAIN;
       }
    }
 }
 
-void TaskLoop::readUnitCycle(QByteArray &unit, SpecialKeyName keyType)
+void TaskLoop::filterBuffer(char* buffer, QString &ret)
+{
+   QString tmp(buffer);
+   if(tmp.at(0) == ASCII_ESC){
+      //匹配命令
+      QList<QString> fnKeys = m_specialKeyMap.keys();
+      QList<QString>::const_iterator it = fnKeys.cbegin();
+      while(it != fnKeys.cend()){
+         if(tmp.startsWith(*it)){
+            ret = *it;
+            break;
+         }
+         it++;
+      }
+      return;
+   }else{
+      int escPos = tmp.indexOf(ASCII_ESC);
+      if(-1 == escPos){
+         ret = tmp;
+      }else{
+         ret = tmp.mid(0, escPos);
+      }
+   }
+}
+
+void TaskLoop::readUnitCycle(QString &unit, SpecialKeyName keyType)
 {
    
    switch(keyType){
@@ -131,6 +154,21 @@ void TaskLoop::readUnitCycle(QByteArray &unit, SpecialKeyName keyType)
       asciiCommand(unit, keyType);
       break;
    }
+   case SpecialKeyName::DEL:
+   {
+      delKeyCommand(unit, keyType);
+      break;
+   }
+   case SpecialKeyName::HOME:
+   {
+      moveToBeginCommand(unit, keyType);
+      break;
+   }
+   case SpecialKeyName::END:
+   {
+      moveToEndCommand(unit, keyType);
+      break;
+   }
    default:
    {
       break;   
@@ -138,31 +176,68 @@ void TaskLoop::readUnitCycle(QByteArray &unit, SpecialKeyName keyType)
    }
 }
 
-void TaskLoop::arrowCommand(QByteArray&, SpecialKeyName keyType)
+void TaskLoop::arrowCommand(QString&, SpecialKeyName keyType)
 {
    QPair<int, int> curPos = getCursorPos();
+   
    if(keyType == SpecialKeyName::ARROW_LEFT){
-      if(((curPos.second * m_windowWidth + curPos.first) - 1) >= (m_cycleBeginY * m_windowWidth + m_cycleBeginX)){
-         Terminal::backwardCursor();
+      int step = 1;
+      if(m_insertPos > 0){
+         QChar curChar = m_cmd_buff[m_insertPos - 1];
+         if(isMultiByteChar(curChar)){
+            step = 2;
+         }
+      }
+      //      QPair<int, int> prevPos = getPrevCursorPos(step);
+      QPair<int, int> prevPos = getPrevCursorPosByInsertPointer();
+      if(curPos != prevPos){
+         Terminal::setCursorPos(prevPos.first, prevPos.second);
          m_insertPos--;
       }
    }else if(keyType == SpecialKeyName::ARROW_RIGHT){
-      QPair<int, int> endPos = getCycleEndCursorPos();
-      if(((curPos.second * m_windowWidth + curPos.first)) <= (endPos.second * m_windowWidth + endPos.first)){
-         Terminal::forwardCursor();
+      int step = 1;
+      int targetPos = qMax(0, qMin(m_insertPos + 1, m_cmd_buff.size() - 1));
+      QChar curChar = m_cmd_buff[targetPos];
+      if(isMultiByteChar(curChar)){
+         step = 2;
+      }
+      QPair<int, int> nextPos = getNextCursorPosByInsertPointer();
+      if(curPos != nextPos){
+         Terminal::setCursorPos(nextPos.first, nextPos.second);
          m_insertPos++;
       }
    }
 }
 
-void TaskLoop::asciiCommand(QByteArray &unit, SpecialKeyName)
+void TaskLoop::asciiCommand(QString &unit, SpecialKeyName)
 {
-   if(unit != "\n"){
-      m_cmd_buff.insert(m_insertPos, QString(unit));
+   if(unit == "\177"){
+      removeCharAtCurrentCursorAction();
+   }else if(unit != "\n"){
+      m_cmd_buff.insert(m_insertPos, unit);
       m_insertPos += unit.size();
-      Terminal::forwardCursor(unit.size());
       refreshLine();
    }
+}
+
+void TaskLoop::delKeyCommand(QString&, SpecialKeyName)
+{
+   removeCharAtCurrentCursorAction();
+}
+
+void TaskLoop::moveToBeginCommand(QString&, SpecialKeyName)
+{
+   Terminal::setCursorPos(m_cycleBeginX, m_cycleBeginY);
+   m_insertPos = 0;
+}
+
+void TaskLoop::moveToEndCommand(QString&, SpecialKeyName)
+{
+   int bufSize = m_cmd_buff.size();
+   int targetX = (m_cycleBeginX + bufSize) % m_windowWidth;
+   int targetY = m_cycleBeginY + (m_cycleBeginX + bufSize) / m_windowWidth;
+   Terminal::setCursorPos(targetX, targetY);
+   m_insertPos = bufSize;
 }
 
 void TaskLoop::refreshLine()
@@ -170,13 +245,55 @@ void TaskLoop::refreshLine()
    Terminal::setCursorPos(m_cycleBeginX, m_cycleBeginY);
    std::cout << "\x1b[J";
    std::cout << m_cmd_buff.toStdString() << std::flush;
-   Terminal::setCursorPos((m_ps.size() + m_insertPos) % m_windowWidth + 1, m_cycleBeginY + (m_insertPos + m_ps.size()) / m_windowWidth);
+   QPair<int, int> targetCursorPos = calculateCursorPosByInsertPointer();
+   Terminal::setCursorPos(targetCursorPos.first, targetCursorPos.second);
 }
 
-TaskLoop::SpecialKeyName TaskLoop::getKeyTypeName(QByteArray& unit)
+QPair<int, int> TaskLoop::calculateCursorPosByInsertPointer()
 {
+   int cycleTotalSize = 0;
+   
+   int winWidth = m_windowWidth;
+   QString buf(m_ps + m_cmd_buff);
+   int totalBufSize = buf.size();
+   int hits = 0;
+   int x;
+   int y;
+   for(int i = 0; i < totalBufSize; i++){
+      int curSize;
+      if(isMultiByteChar(buf[i])){
+         curSize = 2;
+      }else{
+         curSize = 1;
+      }
+      if(winWidth - cycleTotalSize == 1){
+         //这个是否特别判断
+         if(2 == curSize){
+            hits++;
+         }
+         cycleTotalSize = 2;
+         continue;
+      }else if(winWidth == cycleTotalSize){
+         //恰好相等
+         cycleTotalSize = 0;
+         continue;
+      }
+      cycleTotalSize += curSize;
+   }
+   int totalSize = m_ps_length + calculateCursorStep(m_cmd_buff, m_insertPos);
+   x = totalSize % winWidth  + hits;
+   y = m_cycleBeginY + totalSize / winWidth;
+   //m_insertPos += qMax(0, hits - 1);
+   qDebug() << hits;
+   return QPair<int, int>(x, y);
+   
+}
+
+TaskLoop::SpecialKeyName TaskLoop::getKeyTypeName(QString& unit)
+{
+   //进行一些简单的处理
    SpecialKeyName keyType = SpecialKeyName::UNKNOW;
-   if(unit.startsWith(ASCII_ESC) && unit.size() > 1){
+   if(unit.at(0) == ASCII_ESC && unit.size() > 1){
       if(m_specialKeyMap.contains(unit)){
          keyType = m_specialKeyMap[unit];
       }
@@ -186,6 +303,50 @@ TaskLoop::SpecialKeyName TaskLoop::getKeyTypeName(QByteArray& unit)
    return keyType;
 }
 
+void TaskLoop::removeCharAtCurrentCursorAction()
+{
+   QPair<int, int> curPos = getCursorPos();
+   QPair<int, int> prevPos = getPrevCursorPosByInsertPointer();
+   if(curPos != prevPos){
+      m_insertPos--;
+      m_cmd_buff.remove(m_insertPos, 1);
+      refreshLine();
+   }
+}
+
+int TaskLoop::calculateCursorStep(const QString &unit)
+{
+   int step = 0;
+   for(int i = 0; i < unit.size(); i++){
+      if(isMultiByteChar(unit.at(i))){
+         step += 2;
+      }else{
+         step += 1;
+      }
+   }
+   return step;
+}
+
+int TaskLoop::calculateCursorStep(const QString &unit, int end)
+{
+   int step = 0;
+   for(int i = 0; i < end; i++){
+      if(isMultiByteChar(unit.at(i))){
+         step += 2;
+      }else{
+         step += 1;
+      }
+   }
+   return step;
+}
+
+bool TaskLoop::isMultiByteChar(const QChar& unicode)
+{
+   if(unicode.unicode() >= UNICODE_RANGE_START && unicode.unicode() <= UNICODE_RANGE_END){
+      return true;
+   }
+   return false;
+}
 
 QPair<int, int> TaskLoop::getCursorPos()
 {
@@ -211,11 +372,50 @@ void TaskLoop::saveCycleBeginCursorPos()
    m_cycleBeginY = pair.second;
 }
 
+
+
+QPair<int, int> TaskLoop::getPrevCursorPosByInsertPointer()
+{
+   QPair<int, int> pos;
+   int pointer = m_insertPos;
+   if(pointer == 0){
+      pos.first = m_cycleBeginX;
+      pos.second = m_cycleBeginY;
+   }else{
+      pointer--;
+      int totalWidth = m_ps_length + calculateCursorStep(m_cmd_buff, pointer);
+      int x = totalWidth % m_windowWidth + 1;
+      int y = (totalWidth / m_windowWidth) + m_cycleBeginY;
+      pos.first = x;
+      pos.second = y;
+   }
+   return pos;
+}
+
+QPair<int, int> TaskLoop::getNextCursorPosByInsertPointer()
+{
+   QPair<int, int> pos;
+   int pointer = m_insertPos;
+   int stop;
+   if(pointer == m_cmd_buff.size()){
+      stop = pointer;
+   }else{
+      stop = pointer + 1;
+   }
+   int totalWidth = m_ps_length + calculateCursorStep(m_cmd_buff, stop);
+   int x = totalWidth % m_windowWidth + 1;
+   int y = (totalWidth / m_windowWidth) + m_cycleBeginY;
+   pos.first = x;
+   pos.second = y;
+   return pos;
+}
+
 QPair<int, int> TaskLoop::getCycleEndCursorPos()
 {
-   int bufSize = m_cmd_buff.size();
-   int deltaY = bufSize / m_windowWidth;
-   int deltaX = (bufSize % m_windowWidth) + m_ps.size();
+   int psSize = calculateCursorStep(m_ps);
+   int bufSize = calculateCursorStep(m_cmd_buff);
+   int deltaY = (psSize + bufSize) / m_windowWidth;
+   int deltaX = ((bufSize + psSize) % m_windowWidth) + 1;
    return QPair<int, int>(deltaX, m_cycleBeginY + deltaY);
 }
 
@@ -224,7 +424,7 @@ TaskLoop::~TaskLoop()
    tcsetattr(STDIN_FILENO, TCSANOW, &m_savedTerminalAttr);
 }
 
-QMap<QByteArray, TaskLoop::SpecialKeyName> TaskLoop::m_specialKeyMap{
+QMap<QString, TaskLoop::SpecialKeyName> TaskLoop::m_specialKeyMap{
    {"\x1B[H", TaskLoop::SpecialKeyName::HOME},
    {"\x1B[5~", TaskLoop::SpecialKeyName::PAGE_UP},
    {"\x1B[6~", TaskLoop::SpecialKeyName::PAGE_DOWN},
--- src/apps/cloudcontroller/shell/task_loop.h
+++ src/apps/cloudcontroller/shell/task_loop.h
@@ -2,8 +2,8 @@
 #define CLOUD_CONTROLLER_SHELL_TASK_LOOP_H
 
 #include <QMap>
-#include <QByteArray>
 #include <QPair>
+#include <QString>
 
 #include <termios.h>
 #include <unistd.h>
@@ -13,6 +13,7 @@
 
 QT_BEGIN_NAMESPACE
 class QString;
+class QChar;
 QT_END_NAMESPACE
 
 namespace cloudcontroller{
@@ -28,7 +29,7 @@ public:
       ASCII_CODE,
       UNKNOW
    };
-
+   
 public:
    TaskLoop();
    void run();
@@ -37,19 +38,32 @@ public:
 protected:
    void setupTerminalAttr();
    void readCommand(QString& command);
-   void readUnitCycle(QByteArray& unit, SpecialKeyName keyType);
-   SpecialKeyName getKeyTypeName(QByteArray& unit);
+   void readUnitCycle(QString& unit, SpecialKeyName keyType);
+   SpecialKeyName getKeyTypeName(QString& unit);
 protected:
-   void arrowCommand(QByteArray& unit, SpecialKeyName keyType);
-   void asciiCommand(QByteArray& unit, SpecialKeyName keyType);
+   void arrowCommand(QString& unit, SpecialKeyName keyType);
+   void asciiCommand(QString& unit, SpecialKeyName keyType);
+   void delKeyCommand(QString& unit, SpecialKeyName keyType);
+   void moveToBeginCommand(QString& unit, SpecialKeyName keyType);
+   void moveToEndCommand(QString& unit, SpecialKeyName keyType);
+protected:
+   void removeCharAtCurrentCursorAction();
+   QPair<int, int> getPrevCursorPosByInsertPointer();
+   QPair<int, int> getNextCursorPosByInsertPointer();
    void refreshLine();
    QPair<int, int> getCursorPos();
    void saveCycleBeginCursorPos();
    QPair<int, int> getCycleEndCursorPos();
+   QPair<int, int> calculateCursorPosByInsertPointer();
+   void filterBuffer(char* buffer, QString& ret);
+   bool isMultiByteChar(const QChar& unicode);
+   int calculateCursorStep(const QString& unit);
+   int calculateCursorStep(const QString &unit, int end);
+   
 protected:
    struct termios m_savedTerminalAttr;
    QString m_cmd_buff;
-   static QMap<QByteArray, SpecialKeyName> m_specialKeyMap;
+   static QMap<QString, SpecialKeyName> m_specialKeyMap;
    QString m_ps = "cloudcontroller >> ";
    
    int m_insertPos = 0;
@@ -59,6 +73,8 @@ protected:
    
    int m_windowWidth;
    int m_windowHeight;
+   
+   int m_ps_length;
 };
 
 }//shell
